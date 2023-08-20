5bb8a10cf74c8fe37d829505648c4ca80df5277d
unknown <matheuscursino115@gmail.com>
2023-08-20 20:15:51 -0300

    post na rota /login agora consulta se o usuario informado existe no banco de dados

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser');
+var usuarioDoc;
 
-app.use(express.json()) 
-app.use(express.urlencoded({ extended: true }))
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://matheuscursino115:matheus123@auth-database.ngnx8lc.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Ping feito com sucesso. Conectado com o banco de dados.");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+const db = client.db('auth-database');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(bodyParser.json());
+
 
 app.set('view engine', 'ejs');
 app.set('views', './view');
@@ -18,8 +50,15 @@ app.route('/login')
     res.render('login');
   })
   .post(function(req, res) {
-    res.send('');
+    const reqBodyParsed = Object.values(req.body)
+    reqUsuario = reqBodyParsed[0]
+    reqSenha = reqBodyParsed[1]
+    consultar().then( () => {
+        console.log(usuarioDoc)
+    })
+
   })
+
 
 app.route('/registrar')
 .get(function(req, res) {
@@ -29,6 +68,11 @@ app.route('/registrar')
   res.send('');
 })
 
+
+async function consultar() {
+    usuarioDoc =  await db.collection('users').find({usuario: reqUsuario}).toArray()
+  }
+  
 
 app.listen(port, () => {
     console.log(`Servidor iniciado na porta ${port}`)
