3dddb2ac8dfe99a831d196618ea48365606c27b3
unknown <matheuscursino115@gmail.com>
2023-08-29 17:26:19 -0300

    bug fix no POST /registrar

--- controller/registrar.controller.js
+++ controller/registrar.controller.js
@@ -1,5 +1,7 @@
 import usuarioModel from "../model/usuario.model.js"
-
+var reqBodyValores
+var reqUsuario
+var usuarioDoc;
 
 export function RetornarEjs(req, res){
     res.renderFile('../view/registrar.ejs')
@@ -7,22 +9,25 @@ export function RetornarEjs(req, res){
 
 export function RegistrarUsuario(req, res){
     reqBodyValores = Object.values(req.body)
-    reqNome = reqBodyValores[0]
     reqUsuario = reqBodyValores[1]
-    reqSenha = reqBodyValores[2]
 
-    const checarUsuario = usuarioModel({usuario: reqUsuario}).findOne().lean()
-
-    if (checarUsuario.length = 0){
-        const usuario = new usuarioModel(req.body)
-        usuario.save().then(() => {
+    checarUsuario().then(() => {
+         if (usuarioDoc.length = 0){
+            const usuario = new usuarioModel(req.body)
+            usuario.save().then(() => {
+                res
+                    .status(201)
+                    .send("Usuario criado")
+            })
+        } else {
             res
-                .status(201)
-                .send("Usuario criado")
-        })
-    } else {
-        res
-            .status(400)
-            .send("Usuario ja existe")
-    }
+                .status(400)
+                .send("Usuario ja existe")
+        } 
+    })
+}
+
+
+async function checarUsuario(){
+    usuarioDoc = await usuarioModel.findOne({usuario: reqUsuario}).lean()
 }
