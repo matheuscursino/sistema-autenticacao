3080ca3911627471663987039a2ac5f73137600f
unknown <matheuscursino115@gmail.com>
2023-08-29 17:14:12 -0300

    funcao registrarusuario adicionada a rota /registrar no post

--- routes/registrar.route.js
+++ routes/registrar.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
 var registrarRouter = express.Router()
-import { RetornarEjs } from '../controller/registrar.controller.js'
+import { RetornarEjs, RegistrarUsuario } from '../controller/registrar.controller.js'
 
 registrarRouter.get('/', RetornarEjs)
+registrarRouter.post('/', RegistrarUsuario)
 
 export default registrarRouter
