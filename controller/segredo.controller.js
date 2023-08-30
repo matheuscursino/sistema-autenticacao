1b1fa00a7dd7819062c5771486b0781f54677b78
unknown <matheuscursino115@gmail.com>
2023-08-30 20:05:27 -0300

    adicionado erro caso token nao seja autenticado no get /segredo

--- controller/segredo.controller.js
+++ controller/segredo.controller.js
@@ -14,19 +14,23 @@ export default function RetornarEjs(req, res){
   console.log(req.cookies)
     var token = Object.values(req.cookies)
     jwt.verify(token[0], process.env.SEGREDO, function(err, decoded) {
-        id = decoded.usuarioDocId;
-        consultar().then(() => {
-          console.log(usuarioDoc)
-          usuarioDocValores = Object.values(usuarioDoc)
-          nome = usuarioDocValores[1]
-        })
-        res
-            .status(200)
-            .render('../view/segredo.ejs', {
-                nome: nome
-        })
-      }); // to-do: adicionar resposta caso de erro
-    }
+      if (err) {
+        res.status(500)
+           .send('erro ao autenticar token')
+      }
+      id = decoded.usuarioDocId;
+      consultar().then(() => {
+        console.log(usuarioDoc)
+        usuarioDocValores = Object.values(usuarioDoc)
+        nome = usuarioDocValores[1]
+      })
+      res
+          .status(200)
+          .render('../view/segredo.ejs', {
+              nome: nome
+      })
+    }); 
+  }
 }
 
 async function consultar() {
